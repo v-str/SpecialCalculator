41d5d76b40eb1383c1d537fdb5a8656cd26d631d
OrdinaryMind <1.ordinarymind@gmail.com>
2017-02-18 19:51:14 +0300

    Opportunity of different input data was added. With spaces and commas.

--- Calculation/transformer.cpp
+++ Calculation/transformer.cpp
@@ -2,8 +2,38 @@
 
 #include <QString>
 
-double Transformer::TransformString(const QString &value) {
-  QString modyfied_string_value_ = value;
-  modyfied_string_value_.remove(' ');
-  return modyfied_string_value_.toDouble();
+bool Transformer::comma_and_dot_ = false;
+bool Transformer::space_with_comma = false;
+bool Transformer::space_with_dot_ = false;
+bool Transformer::space_ = false;
+bool Transformer::comma_ = false;
+
+double Transformer::TransformString(const QString &string) {
+  QString modyfied_string = string;
+  Analyze(modyfied_string);
+  Transform(modyfied_string);
+  return modyfied_string.toDouble();
+}
+
+void Transformer::Analyze(const QString &string) {
+  comma_and_dot_ = (string.contains(',') && string.contains('.'));
+  space_with_comma = (string.contains(' ') && string.contains(','));
+  space_with_dot_ = (string.contains(' ') && string.contains('.'));
+  space_ = string.contains(' ');
+  comma_ = string.contains(',');
+}
+
+void Transformer::Transform(QString &string) {
+  if (comma_and_dot_) {
+    string.remove(',');
+  } else if (space_with_comma) {
+    string.remove(' ');
+    string.replace(',', '.');
+  } else if (space_with_dot_) {
+    string.remove(' ');
+  } else if (space_) {
+    string.remove(' ');
+  } else if (comma_) {
+    string.replace(',', '.');
+  }
 }
--- Calculation/transformer.h
+++ Calculation/transformer.h
@@ -5,7 +5,17 @@ class QString;
 
 class Transformer {
  public:
-  static double TransformString(const QString &value);
+  static double TransformString(const QString &string);
+
+ private:
+  static void Analyze(const QString &string);
+  static void Transform(QString &string);
+
+  static bool comma_and_dot_;
+  static bool space_with_comma;
+  static bool space_with_dot_;
+  static bool space_;
+  static bool comma_;
 };
 
 #endif  // TRANSFORMER_H
